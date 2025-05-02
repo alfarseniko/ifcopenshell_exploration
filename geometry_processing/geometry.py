a881f70445f27a2937a490e91e49d0d7f34d31a5
Alfa Arseniko <alfarseniko@gmail.com>
2025-05-02 22:46:13 +0200

    geometry

--- geometry_processing/geometry.py
+++ geometry_processing/geometry.py
@@ -9,5 +9,12 @@ settings = geom.settings()
 shape = geom.create_shape(settings, element)
 
 matrix = ios_shape.get_shape_matrix(shape)
+verts = shape.geometry.verts
+edges = shape.geometry.edges
+faces = shape.geometry.faces
 
-print(matrix)
+grouped_verts = ios_shape.get_vertices(shape.geometry)
+grouped_edges = ios_shape.get_edges(shape.geometry)
+grouped_faces = ios_shape.get_faces(shape.geometry)
+
+print(grouped_faces)
